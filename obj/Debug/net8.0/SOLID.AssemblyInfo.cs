3d5db867517593d7b71e26cd2c8941e32a390eef
Matheus Wisch <wisch.pg@outlook.com>
2025-03-07 17:54:17 -0300

    dev

--- obj/Debug/net8.0/SOLID.AssemblyInfo.cs
+++ obj/Debug/net8.0/SOLID.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87829db625c27f5108c33f853fd8eb70addf2b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e10e2532ff0093eca6f7b07a21d9c22221f2dd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
